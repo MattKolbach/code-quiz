a43cc23d3db208d44879203f2cf9baea805b5c7f
Matt Kolbach <mattkolbach@yahoo.com>
2021-12-24 14:26:09 -0600

    added timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,52 @@
+let startQuizButton = document.querySelector("#startQuizButton");
+const initialTime = 5;
+let countDown = initialTime;
+let countDownEl = document.querySelector("#countDownEl");
 
+
+//start button handler start
+var startQuizButtonHandler = function (event) {
+  countDownEl.textContent = initialTime;
+  let intervalId = setInterval(function () {
+    countDown -= 1;
+    countDownEl.textContent = countDown;
+    if (countDown <= 0) {
+      clearInterval(intervalId);
+      countDown = initialTime;
+    }
+
+  }, 1000)
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//event listeners
+startQuizButton.addEventListener("click", startQuizButtonHandler);
 
