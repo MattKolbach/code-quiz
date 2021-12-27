2421ac25c69314f9d9cc31fae5fb63bdc6c91911
Matt Kolbach <mattkolbach@yahoo.com>
2021-12-26 22:57:48 -0600

    zero out, question feedback left to do

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,20 @@ let answer2 = document.querySelector("#answer2");
 let answer3 = document.querySelector("#answer3");
 let answer4 = document.querySelector("#answer4");
 let questionIncriment = 0;
-let finalScoreEl = document.querySelector("#finalScoreEl")
+let finalScoreEl = document.querySelector("#finalScoreEl");
 let intervalId;
+let welcomeSplash = document.querySelector("#welcomeSplash");
+let questionSplash = document.querySelector("#questionSplash");
+let finalScoreSplash = document.querySelector("#finalScoreSplash");
+let initialsForm = document.querySelector("#initialsForm");
+let initials = document.querySelector("#initials");
+let leaderboardArray = [];
+let highScoreSplash = document.querySelector("#highScoreSplash");
+let viewHighScores = document.querySelector("#viewHighScores");
+let goBackToHome = document.querySelector("#goBackToHome");
+let clearHighScores = document.querySelector("#clearHighScores");
+let highScoreList = document.querySelector("#highScoreList");
+
 
 //quiz questions array
 const questionArray = [
@@ -47,13 +59,9 @@ const questionArray = [
   },
 
   {
-    question: "String values must be enclosed within _______ when being assigned to variables.",
-    answers: [
-      "commas",
-      "quotes",
-      "curly brackets",
-      "parenthesis",
-    ],
+    question:
+      "String values must be enclosed within _______ when being assigned to variables.",
+    answers: ["commas", "quotes", "curly brackets", "parenthesis"],
     correctAnswer: 2,
   },
 
@@ -63,11 +71,14 @@ const questionArray = [
     answers: ["<SCRIPT>", "<BODY>", "<HEAD>", "<TITLE>"],
     correctAnswer: 0,
   },
-
 ];
 
 //start button handler start
 var startQuizButtonHandler = function (event) {
+  countDown = initialTime;
+  questionIncriment = 0;
+  welcomeSplash.classList.add("hidden");
+  questionSplash.classList.remove("hidden");
   createQuestionEl();
   countDownEl.textContent = initialTime;
   intervalId = setInterval(function () {
@@ -107,29 +118,82 @@ let checkAnswer = function (event) {
 
   //verify right answer (1button)
   if (answerClicked === questionArray[questionIncriment].correctAnswer) {
-    console.log("correct");
+    //console.log("correct");
   } else {
-    console.log("incorrect");
+    //console.log("incorrect");
+    countDown -= 10;
   }
 
-  if (questionIncriment >= questionArray.length -1){
+  if (questionIncriment >= questionArray.length - 1) {
     clearInterval(intervalId);
-    finalScoreEl.textContent = (countDown + ".");
-console.log(countDown);
-
+    questionSplash.classList.add("hidden");
+    finalScoreSplash.classList.remove("hidden");
+    finalScoreEl.textContent = countDown + ".";
+    countDownEl.textContent = countDown;
+    console.log(countDown);
   } else {
     questionIncriment++;
     createQuestionEl();
   }
-
-
-  //move to next question
-  //splash CORRECT under question div
-  //verify wrong answer
-  //move to next question
-  //remove 10 seconds from timer
-  //splash WRONG under question div
 };
+
+let createLeaderboardEl = function () {
+  leaderboardArray.forEach((highscore) => {
+    let highScoreListEl = document.createElement("li");
+    highScoreListEl.textContent = highscore.initials + "  -  " + highscore.score;
+    highScoreList.appendChild(highScoreListEl);
+  })
+};
+
+let submitInitialsForm = function (event) {
+  event.preventDefault();
+  console.log(leaderboardArray);
+  leaderboardArray.push({ score: countDown, initials: initials.value });
+  initials.value = "";
+  countDownEl.textContent = "";
+  saveLeaderboard();
+  showLeaderboard();
+};
+
+//show the leaderboard
+let showLeaderboard = function () {
+  clearInterval(intervalId);
+  createLeaderboardEl();
+  countDownEl.textContent = "";
+  welcomeSplash.classList.add("hidden");
+  questionSplash.classList.add("hidden");
+  finalScoreSplash.classList.add("hidden");
+  highScoreSplash.classList.remove("hidden");
+};
+
+//show the welcome splash
+let showWelcomeSplash = function () {
+  highScoreSplash.classList.add("hidden");
+  welcomeSplash.classList.remove("hidden");
+  highScoreList.innerHTML = "";
+};
+
+//clear the leaderboard
+let clearLeaderboard = function () {
+  leaderboardArray = [];
+  saveLeaderboard();
+};
+
+
+//save the leaderboard to the local storage
+let saveLeaderboard = function () {
+  localStorage.setItem("leaderboard", JSON.stringify(leaderboardArray.sort((a,b)=>{
+    return b.score - a.score
+  })));
+};
+
+//load the leaderboard to something
+let loadLeaderboard = function () {
+  let loadedLeaderboard = localStorage.getItem("leaderboard");
+  leaderboardArray = JSON.parse(loadedLeaderboard) ?? [];
+};
+
+loadLeaderboard();
 
 //event listeners
 startQuizButton.addEventListener("click", startQuizButtonHandler);
@@ -137,3 +201,12 @@ answer1.addEventListener("click", checkAnswer);
 answer2.addEventListener("click", checkAnswer);
 answer3.addEventListener("click", checkAnswer);
 answer4.addEventListener("click", checkAnswer);
+viewHighScores.addEventListener("click", showLeaderboard);
+goBackToHome.addEventListener("click", showWelcomeSplash);
+clearHighScores.addEventListener("click", clearLeaderboard);
+
+initialsForm.addEventListener("submit", submitInitialsForm);
+
+//hs button hooked up
+//button to go back to welcome
+//button to clear leaderboard
